3c00312d6249b685e39567bd5b59f0888879c5f3
David Stinnette <dastinnette@gmail.com>
2015-09-07 14:19:21 -0600

    Add application details controller and find new source feature tests

---
+++ test/controllers/application_details_test.rb
@@ -0,0 +1,26 @@
+require './test/test_helper'
+
+class ApplicationDetailsTest<Minitest::Test
+  include Rack::Test::Methods
+  include TrafficSpy
+
+  def setup
+    post "/sources/:identifier/data"
+    @data1 = 'payload={"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
+    @data2 = 'payload={"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.8.211"}'
+    @data3 = 'payload={"url":"http://jumpstartlab.com/article/1","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
+  end
+
+  def test_it_can_parse_payload
+    post "/sources", client
+    post "/sources/jumpstartlab/data", @data1
+    post "/sources/jumpstartlab/data", @data2
+    post "/sources/jumpstartlab/data", @data3
+
+    assert_equal 3, Payload.count
+    assert_equal 200, last_response.status
+  end
+
+  
+
+end

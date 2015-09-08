c0d1f3a1f51ad58653ea52d985cf532b5e7f5059
David Stinnette <dastinnette@gmail.com>
2015-09-08 08:14:37 -0600

    Add resolution assertions to source feature test

--- test/features/find_a_new_source_test.rb
+++ test/features/find_a_new_source_test.rb
@@ -8,6 +8,7 @@ class FindNewSourceTest < FeatureTest
     @data1 = 'payload={"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
     @data2 = 'payload={"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.8.211"}'
     @data3 = 'payload={"url":"http://jumpstartlab.com/article/1","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
+    @data4 = 'payload={"url":"http://jumpstartlab.com/article/1","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"80","ip":"63.29.38.211"}'
   end
 
   def test_it_can_find_the_navbar
@@ -52,6 +53,7 @@ class FindNewSourceTest < FeatureTest
     post "/sources/jumpstartlab/data", @data1
     post "/sources/jumpstartlab/data", @data2
     post "/sources/jumpstartlab/data", @data3
+    post "/sources/jumpstartlab/data", @data4
     visit "/sources"
     click_link("Jumpstartlab")
 
@@ -59,7 +61,8 @@ class FindNewSourceTest < FeatureTest
     assert page.has_content?("Macintosh")
     assert page.has_content?("jumpstartlab")
     assert page.has_content?("http://jumpstartlab.com/blog : Was requested 2 time(s)")
-    assert page.has_content?("http://jumpstartlab.com/article/1 : Was requested 1 time(s)")
-    assert page.has_content?("")
+    assert page.has_content?("http://jumpstartlab.com/article/1 : Was requested 2 time(s)")
+    assert page.has_content?("1280 x 1920")
+    assert page.has_content?("80 x 1920")
   end
 end
